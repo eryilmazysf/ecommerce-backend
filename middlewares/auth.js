610fe741413d610c04507879097a8b725eab5319
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2021-03-09 11:00:38 -0500

    cont-routes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,5 @@
 const admin = require("../firebase");
+const User = require("../models/user");
 
 exports.authCheck = async (req, res, next) => {
   //console.log(req.headers); //token
@@ -11,5 +12,17 @@ exports.authCheck = async (req, res, next) => {
     next();
   } catch (err) {
     res.status(401).json({ err: "Invalid or expired token" });
+  }
+};
+
+exports.adminCheck = async (req, res, next) => {
+  const { email } = req.user;
+  const adminUser = await User.findOne({ email }).exec();
+  if (adminUser.role !== "admin") {
+    res.status(403).json({
+      err: "Admin resource. Access denied.",
+    });
+  } else {
+    next();
   }
 };
